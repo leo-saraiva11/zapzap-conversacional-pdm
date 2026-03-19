35f2fbb6e8b6fa7535f63ee663a4b32f8543000a
leo-saraiva11 <contatoleonardosaraiva@gmail.com>
2026-03-19 03:23:29 -0300

    chore: remover pasta build do rastreamento

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_zapzap_ZapZapApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.zapzap.ZapZapApplication"
-)
-public final class _com_example_zapzap_ZapZapApplication {
-}
